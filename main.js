1d08a7e43d0f52e143711abcc47a1d388d4f8bab
Eunhye, Son <85356011+eunne@users.noreply.github.com>
2021-07-29 20:24:12 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,65 @@
-const input = document.querySelector("#input");
-const add = document.querySelector("#add");
-const list = document.querySelector("#list");
-const body = document.querySelector("body");
+const items = document.querySelector(".items")
+const addBtn = document.querySelector(".footer__btn");
+const input = document.querySelector(".footer__input");
 
-add.addEventListener("click", clickButton);
-input.addEventListener("keyup", event => {
-  if (event.keyCode === 13 ){
-    clickButton();
-    }
+
+function onAdd() {
+  //사용자가 입력한 텍스트를 받아온다
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
   }
-);
-body.addEventListener("resize", resize)
-
-
-
-let idx = 0;
-
-function clickButton(){
-  let temp = document.createElement('div');
-  temp.setAttribute("class","alllist");
-  temp.setAttribute("id","addlist"+idx);
-  temp.innerHTML = input.value;
-  temp.innerHTML += "<button id='addbtn' onclick='remove("+idx+")'>X</button>";
-  list.appendChild(temp);
+  // 리스트에 입력한 내용이 추가된다
+  const item = createItem(text);
+  items.appendChild(item);
+  // input창이 리셋됨
   input.value = '';
-  idx++
+  // 반복적으로 입력할 수 있도록 커서가 남아있음
+  input.focus();
 }
 
-function remove(idx) {
-  let temp = document.getElementById('addlist'+idx)
-  list.removeChild(temp)
+
+function createItem(text) {
+  if (text != '') {
+
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+    
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
+
+  const itemName = document.createElement("span");
+  itemName.setAttribute("class", "item_name");
+  itemName.innerHTML = text;
+  
+  const itemDelete = document.createElement("button");
+  itemDelete.setAttribute("class","item__delete");
+  itemDelete.innerHTML = `<i class="far fa-minus-square"></i>`;
+  //delete 버튼을 누르면 삭제됨
+  itemDelete.addEventListener("click", () => {
+    items.removeChild(itemRow);
+  })
+
+  const itemDivider = document.createElement("div");
+  itemDivider.setAttribute("class","item__underline");
+
+  item.appendChild(itemName);
+  item.appendChild(itemDelete);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+
+  return itemRow;
+}
 }
 
-function resize(event) { 
-  body.style.
-  body.style.transform = `translate(${event.clientX}px + ${50}%, ${event.clientY}px)`
-}
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", event => {
+  if (event.keyCode === 13) {
+    onAdd();
+  }
+})
